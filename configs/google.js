66a80002ae72bea38cdac7f74725cd42fee29d2e
hhtjohnson01 <henryjohnson@Henrys-MacBook-Air.local>
2017-06-28 23:02:30 -0400

    removing configs

--- configs/google.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by henryjohnson on 6/22/17.
- */
-const Google = {
-    API_KEY: 'AIzaSyDEGgntDKjVkTbI0c_qpoL-C3EC0xfEd70',
-    PLACES: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDEGgntDKjVkTbI0c_qpoL-C3EC0xfEd70&libraries=places&callback=initMap"
-}
-
-module.exports = Google
